1f155d2b2280eafd5eb7ea74fdf4382f9250932b
Andrey <titovandrey0202@mail.ru>
2022-07-31 19:30:39 +0300

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,39 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static org.testng.Assert.assertEquals;
+
+@Listeners(MyListener.class)
+
+public class Main {
+    public enum Priority {
+        Blocker, Critical, Major, Minor
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.TYPE, ElementType.METHOD}) //on class level
+    public @interface TestMethodInfo {
+
+        //Приоритет теста
+        Priority priority() default Priority.Major;
+
+        //Автор теста
+        String author() default "Bill Gates";
+
+        //Дата последних изменений в тесте
+        String lastModified() default "01.01.2019";
+    }
+
+    @Test
+    @TestMethodInfo(priority = Priority.Critical, author = "Test user", lastModified = "20.08.2019")
+    public void annotation() {
+        assertEquals(1, 1);
     }
 }
--- src/main/java/org/example/MyListener.java
+++ src/main/java/org/example/MyListener.java
@@ -1,2 +1,38 @@
-package org.example;public class MyListener {
+package org.example;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+
+import java.lang.reflect.Method;
+
+public class MyListener implements ISuiteListener {
+
+    @Override
+    public void onStart(ISuite iSuite) {
+        Method[] methods = Main.class.getMethods();
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(Main.TestMethodInfo.class)) {
+                Main.TestMethodInfo ta = m.getAnnotation(Main.TestMethodInfo.class);
+                System.out.println("Start tests");
+                System.out.println(ta.priority());
+                System.out.println(ta.author());
+                System.out.println(ta.lastModified());
+
+            }
+        }
+
+    }
+
+    @Override
+    public void onFinish(ISuite iSuite) {
+        Method[] methods = Main.class.getMethods();
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(Main.TestMethodInfo.class)) {
+                Main.TestMethodInfo ta = m.getAnnotation(Main.TestMethodInfo.class);
+                System.out.println("Finish Tests");
+                System.out.println(ta.priority());
+                System.out.println(ta.author());
+                System.out.println(ta.lastModified());            }
+        }
+    }
 }
